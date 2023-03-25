b12b24dd87ef80f9df379aef45e604b57dc8d273
Fulya Sarıyar <fulyasariyar@outlook.com>
2023-03-25 19:46:26 +0300

    S. ok

---
+++ Single_Responsibility_Principle/IdealCode.cs
@@ -0,0 +1,29 @@
+﻿using Single_Responsibility_Principle;
+
+namespace Single_Responsibility_Principle_IdealCode;
+class Database
+{
+    public void Connect()
+    {
+        //...
+        Console.WriteLine("Veritabanı bağlantısı sağlanmıştır.");
+    }
+    public void Disconnect()
+    {
+        //...
+        Console.WriteLine("Veritabanı bağlantısı kesilmiştir.");
+    }
+    public string State { get; set; }
+}
+
+class PersonService
+{
+    public List<Person> GetPersons()
+    {
+        return new() {
+            new(){ Name = "Alpaçino", Surname = "GodFather" },
+            new(){ Name = "Alpaçino", Surname = "Skyface" },
+            new(){ Name = "Apollonia", Surname = "Vitelli Corleone" }
+        };
+    }
+}
---
+++ Single_Responsibility_Principle/NotIdealCode.cs
@@ -0,0 +1,26 @@
+﻿using Single_Responsibility_Principle;
+
+namespace Single_Responsibility_Principle_NotIdealCode;
+
+class Database
+{
+    public void Connect()
+    {
+        //...
+        Console.WriteLine("Veritabanı bağlantısı sağlanmıştır.");
+    }
+    public void Disconnect()
+    {
+        //...
+        Console.WriteLine("Veritabanı bağlantısı kesilmiştir.");
+    }
+    public string State { get; set; }
+    public List<Person> GetPersons()
+    {
+        return new() {
+            new(){ Name = "Alpaçino", Surname = "GodFather" },
+            new(){ Name = "Alpaçino", Surname = "Skyface" },
+            new(){ Name = "Apollonia", Surname = "Vitelli Corleone" }
+        };
+    }
+}
